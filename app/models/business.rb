bfd740cdc7f76421233de5ac5e6cd8e74d6509e7
steve lawson <stevelawsonspi@gmail.com>
2019-06-16 13:14:45 +1000

    has_many departments

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,5 +1,6 @@
 class Business < ApplicationRecord
   belongs_to :user
+  has_many   :departments
   has_many   :employees
   has_many   :phone_numbers, as: :phone_numberable
   has_many   :emails,        as: :emailable
